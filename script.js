6e33c324f35fb7dcdd48ae9dc808d12ae49413c2
raquelpicancooliveira <raquel.picanco.oliveira@escola.pr.gov.br>
2026-06-16 07:54:45 -0300

    borabill

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function mostrarMensagem() {
     alert("Obrigado por visitar o site Agro Forte, Futuro Sustentável!");
 }
 
-console.log("Site carregado com sucesso!");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Site carregado com sucesso!");
+});
