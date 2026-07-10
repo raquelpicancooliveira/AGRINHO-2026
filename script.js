9cd6ca118042ced815ce791741148c33760b7a02
raquelpicancooliveira <raquel.picanco.oliveira@escola.pr.gov.br>
2026-07-10 14:55:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function mostrarMensagem() {
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Site carregado com sucesso!");
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const imagens = document.querySelectorAll("img");
+
+    imagens.forEach(function (imagem) {
+        imagem.addEventListener("click", function () {
+            alert("Esta imagem representa práticas sustentáveis no agronegócio!");
+        });
+    });
+});
